3d7b21fb6a75f3337a066bd7a98c5eac2c563ee8
Juan Pereira Robi <juanpereirarobi@gmail.com>
2025-10-03 16:14:08 -0300

    portfólio 0.1

--- app.rb
+++ app.rb
@@ -1,10 +1,26 @@
 require "sinatra"
 require "rack"
 
+# Rota para home
 get "/" do
+    redirect "/home"
+end
+
+get "/home" do
     erb :index
 end
 
+# Rota projetos
 get "/projetos" do
     erb :projetos
+end
+
+# Rota para o projeto form
+get "/projetos/teste_form" do
+    erb :projeto_descricao_teste_form
+end
+
+# Rota para project-atv2
+get "/projetos/project_atv2" do
+    erb :projeto_descricao_project_atv2
 end
